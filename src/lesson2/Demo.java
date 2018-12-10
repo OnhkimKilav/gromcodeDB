5c712eb7a3820da01e0860d0b1d94f5257bd77a9
OnhkimKilav <mihno.valik@mail.ru>
2018-12-10 16:06:51 +0300

    db

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+/**
+ * Created by Valik on 10.12.2018.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        //java
+            //create class with fields
+            //create object with some values
+            //using it
+
+
+        //db
+            //create table with columns
+            //insert some date to the table
+            //using it
+    }
+}
